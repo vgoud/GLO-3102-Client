5a26b975a3525484c3037a7009551dcef71b4c58
Vincent <vincent.goudeault.1@gmail.com>
2014-09-27 17:14:54 -0400

    Improvements in the songs' table in album.html. Animation of the icon for the currently playing song. Added jquery file for auto-completion in webstorm; we'll have to remove it in production.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,35 @@
  * Created by Vincent on 2014-09-25.
  */
 
-//$(document).ready( function() {
-//    var $navbar = $("#navbar");
-//    var sticky = $.UIkit.sticky($navbar);
-//});
+$(document).ready(function () {
+    // Handlers of toggle-type play buttons in the table.
+    $(".ub-button-play").click(function () {
+        var $el = $(this);
+        togglePlayVolumeIcons($el);
+    });
+
+    var animationPlayButtonClasses = "animated infinite pulse";
+
+    function togglePlayVolumeIcons($el) {
+        if ($el.hasClass("ub-active")) {
+            $el.removeClass("ub-active " + animationPlayButtonClasses);
+//            stopSong();
+        }
+        else {
+            // We can only have one song playing at once.
+            var $playingSongButton = $(".ub-button-play.ub-active");
+            if ($playingSongButton) {
+                $playingSongButton.removeClass("ub-active " + animationPlayButtonClasses);
+            }
+            // We remove the track number and replace it by an icon of volume.
+            $el.addClass("ub-active " + animationPlayButtonClasses);
+//            playSong();
+        }
+    }
+    function playSong() {
+        return;
+    }
+    function stopSong() {
+        return;
+    }
+});
