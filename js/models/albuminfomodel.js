16d89bd7350b3fd545eef1386aac4f0315741476
Vincent <vincent.goudeault.1@gmail.com>
2014-10-29 12:16:59 -0400

    AlbumInfoView now displays a 400x400 thumbnail.

--- js/models/albuminfomodel.js
+++ js/models/albuminfomodel.js
@@ -6,6 +6,8 @@ window.UB.Models.AlbumInfoModel = Backbone.Model.extend({
     defaults: {
         artworkUrl60: "",
         artworkUrl100: "",
+        artworkUrl200: "",
+        artworkUrl400: "",
         collectionName: "n/d",
         trackCount: "n/d",
         copyright: "n/d",
@@ -24,6 +26,26 @@ window.UB.Models.AlbumInfoModel = Backbone.Model.extend({
     },
 
     parse: function(data) {
-        return data.results[0];
+
+        var albumInfo = data.results[0];
+
+        var extension =
+            albumInfo.artworkUrl100.substr(
+                albumInfo.artworkUrl100.lastIndexOf("."),
+                albumInfo.artworkUrl100.length);
+        var albumInfoWithSize =
+            albumInfo.artworkUrl100.substr(
+                0,
+                albumInfo.artworkUrl100.lastIndexOf("."));
+        albumInfo.artworkUrl200 =
+            albumInfoWithSize.substr(
+                0,
+                albumInfoWithSize.lastIndexOf(".")) + ".200x200-75" + extension;
+        albumInfo.artworkUrl400 =
+            albumInfoWithSize.substr(
+                0,
+                albumInfoWithSize.lastIndexOf(".")) + ".400x400-75" + extension;
+
+        return albumInfo;
     }
 });
