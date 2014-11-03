2ca1202082c26f42603145d63a90e0a91ae00fbc
MICOU159 <michel.couture.3@ulaval.ca>
2014-11-03 15:06:26 -0500

    Added some code to the playlistView (incomplete)

--- js/views/playlistview.js
+++ js/views/playlistview.js
@@ -4,4 +4,18 @@
 
 window.UB.Views.PlaylistView = Backbone.View.extend({
 
+    tagName: "ul",
+    className: "uk list uk-list-line",
+
+    initialize: function () {
+        this.listenTo(this.collection, "change add sync", this.render);
+    },
+
+    //Il va y avoir du traitement à faire pour aller chercher uniquement les playlists de l'utilisateur courant.
+    render: function() {
+        $(this.el).html(this.template({
+            playlists: this.collection.toJSON()
+        }));
+        return this;
+    }
 });
