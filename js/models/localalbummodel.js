fa4c5dc2d78a986f91d0cd3f74f922b9d7d5586b
sgalarneau <sgalarneau@gmail.com>
2014-12-12 22:03:38 -0500

    Added for real

---
+++ js/models/localalbummodel.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Sebastien on 12/11/2014.
+ */
+
+window.UB.Models.LocalAlbumModel = UB.Models.AlbumModel.extend( {
+
+    parse: function (data) {
+
+        var albumInfo = data;
+
+        var extension =
+            albumInfo.artworkUrl100.substr(
+                albumInfo.artworkUrl100.lastIndexOf("."),
+                albumInfo.artworkUrl100.length);
+        var albumInfoWithSize =
+            albumInfo.artworkUrl100.substr(
+                0,
+                albumInfo.artworkUrl100.lastIndexOf("."));
+        albumInfo.artworkUrl200 =
+            albumInfoWithSize.substr(
+                0,
+                albumInfoWithSize.lastIndexOf(".")) + ".200x200-75" + extension;
+        albumInfo.artworkUrl400 =
+            albumInfoWithSize.substr(
+                0,
+                albumInfoWithSize.lastIndexOf(".")) + ".400x400-75" + extension;
+
+        return data;
+    }
+});
